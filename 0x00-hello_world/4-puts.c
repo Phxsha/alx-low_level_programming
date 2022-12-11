b3741e07e2011bf4d23f41507d9a25d566be9ce9
Phxsha <phashateboho@gmail.com>
2022-12-11 06:48:23 -0800

    puts program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char Hello = ""Programming is like building a multilingual puzzle";
+
+	puts(Hellon\n);
+	return (0);
+}
