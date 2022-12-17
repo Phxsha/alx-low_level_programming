2bdb3d922f279cb964ebff135123b974fc3d2443
Phxsha <phashateboho@gmail.com>
2022-12-17 13:33:56 -0800

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Return: returns 1 if TRUE returns 0 if FALSE
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+}
