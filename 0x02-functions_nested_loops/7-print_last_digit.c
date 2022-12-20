949bd3dccdd363be582c39aea71c775f64506131
Phxsha <phashateboho@gmail.com>
2022-12-20 05:23:46 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,15 @@ int print_last_digit(int d)
 
 	if (d < 0)
 	{
-		last_digit = (-1 * (n % 10));
+		last_digit = (-1 * (d % 10));
 
-		_putchar(laat_digit + '0');
+		_putchar(last_digit + '0');
 
 		return (last_digit);
 	}
 	else
 	{
-		last digit = (n % 10);
+		last_digit = (d % 10);
 
 		_putchar(last_digit + '0');
 
