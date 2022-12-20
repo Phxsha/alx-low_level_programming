f1cd81dbfab37124134529e149432823d3ceea65
Phxsha <phashateboho@gmail.com>
2022-12-20 11:11:57 -0800

    add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns result
+ * @a: assigned int
+ * @b: assigned int
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
