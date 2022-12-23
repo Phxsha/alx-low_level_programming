9911d949be2890bbc86e85e69584571dcd740e30
Phxsha <phashateboho@gmail.com>
2022-12-23 07:44:20 -0800

    upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: assigned int
+ * Return: 1 if TRUE and 0 if FALSE
+ */
+int _isupper(int c)
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
