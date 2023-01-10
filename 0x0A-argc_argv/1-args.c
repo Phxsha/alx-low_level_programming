38a28d4864835da79ddd1b5cb0a0dc42c856508b
Phxsha <phashateboho@gmail.com>
2023-01-10 11:42:21 -0800

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed int it
+ * @argc: array size
+ * @argv: number of elements in it
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	printf("'%d\n", argc);
+
+	return (0);
+}
