698f8fb01783565a19b75add9efaa36e287fb7dd
Phxsha <phashateboho@gmail.com>
2023-10-02 14:33:37 -0700

    Creates a fill and truncates with specific permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	bytes_written = write(fd, text_content, _strlen(text_content));
 	close(fd);
-	if (len == -1)
+	if (bytes_written == -1)
 		return (-1);
 	return (1);
 }
