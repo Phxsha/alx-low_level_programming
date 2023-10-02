fcfb88cba330b4bcce4e7e13b71d6bcb3373729f
Phxsha <phashateboho@gmail.com>
2023-10-02 14:46:05 -0700

    Create a file with specific permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,8 @@ size_t _strlen(char *str)
 	size_t i;
 
 	for (i = 0; str[i]; i++)
-		return (i);
+		;
+	return (i);
 }
 
 /**
@@ -25,17 +26,17 @@ size_t _strlen(char *str)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes_written = 0;
+	ssize_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
-	bytes_written = write(fd, text_content, _strlen(text_content));
+	len = write(fd, text_content, _strlen(text_content));
 	close(fd);
-	if (bytes_written == -1)
+	if (len == -1)
 		return (-1);
 	return (1);
 }
